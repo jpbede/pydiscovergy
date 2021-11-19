d27ca73d1ced04fbc080544e60ecdf6e5ca833f1
Jan-Philipp Benecke <jan-philipp@bnck.me>
2021-11-19 19:51:51 +0100

    fix readme in setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     author='Jan-Philipp Benecke',
     author_email='jan-philipp.benecke@jpbe.de',
     description='Async Python 3 library for interacting with Discovergy smart meters API',
-    long_description="file: README.md",
+    long_description="file:README.md",
     long_description_content_type="text/markdown",
     install_requires=['httpx', 'authlib'],
     python_requires='>=3.6'

08aad97e6b530b41bc43d6697ba217ecc6d65507
Jan-Philipp Benecke <jan-philipp@bnck.me>
2024-01-25 18:10:30 +0100

    Migrate dataclasses-json to mashumaro

--- pydiscovergy/models.py
+++ pydiscovergy/models.py
@@ -1,44 +1,55 @@
 """Models for Discovergy API."""
 from dataclasses import dataclass, field
 from datetime import datetime
+from typing import Any, Self
 
+from mashumaro.mixins.orjson import DataClassORJSONMixin
+from mashumaro.types import SerializationStrategy
 import pytz
-from dataclasses_json import CatchAll, LetterCase, Undefined, config, dataclass_json
-from marshmallow import fields
 
 
-@dataclass_json(letter_case=LetterCase.CAMEL)
+class MillisecondTimestampStrategy(SerializationStrategy, use_annotations=True):
+    """Serialization strategy for timestamps with milliseconds."""
+
+    def serialize(self, value: datetime) -> float:
+        """Serialize a datetime to an timestamp."""
+        return value.timestamp() * 1000
+
+    def deserialize(self, value: float) -> datetime:
+        """Deserialize an timestamp to a datetime."""
+        return datetime.fromtimestamp(value / 1000)
+
+
 @dataclass(frozen=True, slots=True)
-class Location:
+class Location(DataClassORJSONMixin):
     """Represents a smart meter location."""
 
     street: str
-    street_number: str
+    street_number: str = field(metadata={"alias": "streetNumber"})
     city: str
     zip: int
     country: str
 
 
-@dataclass_json
 @dataclass(frozen=True, slots=True)
-class Reading:
+class Reading(DataClassORJSONMixin):
     """Represents a reading of a smart meter."""
 
-    time: datetime = field(metadata=config(mm_field=fields.DateTime("timestamp_ms")))
-    values: dict[str, float] = field(
-        metadata=config(
-            mm_field=fields.Dict(keys=fields.String(), values=fields.Float())
-        )
-    )
+    time: datetime
+    values: dict[str, float]
 
     @property
     def time_with_timezone(self) -> datetime:
         return pytz.timezone("UTC").localize(self.time)
 
+    class Config:
+        serialization_strategy = {
+            datetime: MillisecondTimestampStrategy(),
+        }
 
-@dataclass_json
+
 @dataclass(frozen=True, slots=True)
-class Statistic:
+class Statistic(DataClassORJSONMixin):
     """Represents a meter statistic entry."""
 
     count: int
@@ -48,16 +59,50 @@ class Statistic:
     variance: float
 
 
-@dataclass_json(undefined=Undefined.INCLUDE, letter_case=LetterCase.CAMEL)
 @dataclass(frozen=True, slots=True)
 class Meter:
     """Represents a smart meter."""
 
-    meter_id: str
-    serial_number: str
-    full_serial_number: str
+    meter_id: str = field(metadata={"alias": "meterId"})
+    serial_number: str = field(metadata={"alias": "serialNumber"})
+    full_serial_number: str = field(metadata={"alias": "fullSerialNumber"})
     type: str
-    measurement_type: str
-    load_profile_type: str
+    measurement_type: str = field(metadata={"alias": "measurementType"})
+    load_profile_type: str = field(metadata={"alias": "loadProfileType"})
     location: Location
-    additional: CatchAll
+    additional: dict[str, Any]
+
+    @classmethod
+    def __pre_deserialize__(
+        cls: type[Self],
+        d: dict[Any, Any],
+    ) -> dict[Any, Any]:
+        """Raise when response was unexpected."""
+        non_additional = {
+            "meterId",
+            "serialNumber",
+            "fullSerialNumber",
+            "type",
+            "measurementType",
+            "loadProfileType",
+            "location",
+        }
+        return {
+            **d,
+            "additional": {k: v for k, v in d.items() if k not in non_additional},
+        }
+
+
+@dataclass(frozen=True, slots=True)
+class MetersResponse(DataClassORJSONMixin):
+    """Represents a response with all meters."""
+
+    meters: list[Meter]
+
+    @classmethod
+    def __pre_deserialize__(
+        cls: type[Self],
+        d: dict[Any, Any],
+    ) -> dict[Any, Any]:
+        """Raise when response was unexpected."""
+        return {"meters": d}
